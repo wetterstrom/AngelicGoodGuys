4074434caf2066f4c045add0a4c6176719b08190
wetterstrom <robinwetterstrom@gmail.com>
2025-12-08 11:07:36 +0100

    Sprint 1

--- AngelicGoodGuys/Program.cs
+++ AngelicGoodGuys/Program.cs
@@ -1,11 +1,65 @@
-﻿namespace AngelicGoodGuys
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AngelicGoodGuys
 {
-    internal class Program
+    public enum Gender
     {
+        Male,
+        Female,
+        NonBinary,
+        Other
+    }
+
+    public struct Hair
+    {
+        // Make fields public properties and correct spelling
+        public string Length { get; set; }
+        public string Color { get; set; }
+
+    }
+
+
+
+    public class Person
+    {
+        public Gender Gender { get; set; }
+        public Hair Hair { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EyeColor { get; set; }
+
+        public override string ToString()
+        {
+            return $"Gender: {Gender}\n" +
+                   $"Hair: {Hair.Color}, {Hair.Length}\n" +
+                   $"Birthday: {DateOfBirth.ToShortDateString()}\n" +
+                   $"Eye color: {EyeColor}";
+        }
+    }
+
+    class Program
+    {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Angelic Good Guys application!");
-            Console.WriteLine();
+            Hair suspectHair = new Hair
+            {
+                Length = "Long",
+                Color = "Blonde"
+            };
+
+            Person suspect = new Person
+            {
+                Gender = Gender.Female,
+                Hair = suspectHair,
+                DateOfBirth = new DateTime(1999, 3, 16),
+                EyeColor = "Blue"
+            };
+
+            Console.WriteLine($"This is the collected data from sprint 1: ");
+            Console.WriteLine(suspect.ToString());
+            Console.ReadKey();
         }
     }
 }
